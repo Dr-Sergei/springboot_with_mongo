7af982482534646ff01e4e746f55f39c5f8826df
Wittchen <swittche@ipb-halle.de>
2024-05-28 12:57:34 +0200

    phase 0.5: set-up

---
+++ src/main/java/com/sergei/spring/mongo/myspring_with_mongo/MyspringWithMongoApplication.java
@@ -0,0 +1,13 @@
+package com.sergei.spring.mongo.myspring_with_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringWithMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringWithMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sergei/spring/mongo/myspring_with_mongo/MyspringWithMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergei.spring.mongo.myspring_with_mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringWithMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
